3c173c0fe7ec09b8eb12f04ef03e359a91c809d3
LabPBOA2023 <126652888+LabPBOA2023@users.noreply.github.com>
2023-03-25 08:36:38 +0700

    Menambahkan Import Class Kubus
    Dalam Latihan Praktikum 4 ini kita harus memanfaatkan class Kubus untuk menghitung luas alas dan volume.

--- Pertemuan 4/direktori_anda/org/main/MKubus.java
+++ Pertemuan 4/direktori_anda/org/main/MKubus.java
@@ -6,10 +6,12 @@ Deskripsi : driver class untuk poligon dan kubus
 package org.main;
 
 import org.bangundatar.*;
+import org.bangundatar.BujurSangkar; // TODO 1 harus memanfaatkan class Kubus untuk menghitung luas alas dan volume
 
 public class MKubus{
     public static void main (String[] args) {
         BujurSangkar bujursangkar = new BujurSangkar(4.0);
+        // TODO 2 inisiasi variable dengan menggunakan class Kubus
         System.out.println("Luas Permukaan Kubus dengan panjang sisi 4 satuan: "+bujursangkar.hitungLuas());
 
         BujurSangkar bujursangkar2 = new BujurSangkar(8.0);
